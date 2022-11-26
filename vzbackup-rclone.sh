2ee8bcbaf8073b3503d18927fb3d9fe78d050371
Simon Caron <simon.caron.8@gmail.com>
2022-11-25 23:18:48 -0500

    Merge Updates from PRs in source repo

--- vzbackup-rclone.sh
+++ vzbackup-rclone.sh
@@ -1,30 +1,36 @@
 #!/bin/bash
 # ./vzbackup-rclone.sh rehydrate YYYY/MM/DD file_name_encrypted.bin
-
 ############ /START CONFIG
 dumpdir="/mnt/pve/pvebackups01/dump" # Set this to where your vzdump files are stored
 MAX_AGE=3 # This is the age in days to keep local backup copies. Local backups older than this are deleted.
+REMOTE_NAME=gd-backup_crypt
+SUB_PATH=/proxmox/
+CONFIG_PATH=/root/.config/rclone/rclone.conf
 ############ /END CONFIG
 
 _bdir="$dumpdir"
 rcloneroot="$dumpdir/rclone"
 timepath="$(date +%Y)/$(date +%m)/$(date +%d)"
+destpath="${REMOTE_NAME}:${SUB_PATH}${timepath}"
 rclonedir="$rcloneroot/$timepath"
 COMMAND=${1}
 rehydrate=${2} #enter the date you want to rehydrate in the following format: YYYY/MM/DD
 if [ ! -z "${3}" ];then
         CMDARCHIVE=$(echo "/${3}" | sed -e 's/\(.bin\)*$//g')
 fi
-tarfile=${TARFILE}
+
+if [ -z ${TARGET+x} ]; then 
+    tarfile=${TARFILE}
+else
+    tarfile=${TARGET}
+fi
+
 exten=${tarfile#*.}
 filename=${tarfile%.*.*}
 
 if [[ ${COMMAND} == 'rehydrate' ]]; then
-    #echo "Please enter the date you want to rehydrate in the following format: YYYY/MM/DD"
-    #echo "For example, today would be: $timepath"
-    #read -p 'Rehydrate Date => ' rehydrate
-    rclone --config /root/.config/rclone/rclone.conf \
-    --drive-chunk-size=32M copy gd-backup_crypt:/$rehydrate$CMDARCHIVE $dumpdir \
+    rclone --config ${CONFIG_PATH} \
+    --drive-chunk-size=32M copy ${destpath}/$rehydrate$CMDARCHIVE $dumpdir \
     -v --stats=60s --transfers=16 --checkers=16
 fi
 
@@ -35,12 +41,10 @@ fi
 
 if [[ ${COMMAND} == 'backup-end' ]]; then
     echo "Backing up $tarfile to remote storage"
-    #mkdir -p $rclonedir
-    #cp -v $tarfile $rclonedir
     echo "rcloning $rclonedir"
-    #ls $rclonedir
-    rclone --config /root/.config/rclone/rclone.conf \
-    --drive-chunk-size=32M copy $tarfile gd-backup_crypt:/$timepath \
+
+    rclone --config ${CONFIG_PATH} \
+    --drive-chunk-size=32M copy $tarfile ${destpath} \
     -v --stats=60s --transfers=16 --checkers=16
 fi
 
@@ -70,15 +74,10 @@ if [[ ${COMMAND} == 'job-end' ||  ${COMMAND} == 'job-abort' ]]; then
     # archive the copied system files
     tar -cvzPf "$_filename4" $_tdir/*.tar
 
-    # copy config archive to backup folder
-    #mkdir -p $rclonedir
     cp -v $_filename4 $_bdir/
-    #cp -v $_filename4 $rclonedir/
     echo "rcloning $_filename4"
-    #ls $rclonedir
-    rclone --config /root/.config/rclone/rclone.conf \
-    --drive-chunk-size=32M move $_filename4 gd-backup_crypt:/$timepath \
-    -v --stats=60s --transfers=16 --checkers=16
 
-    #rm -rfv $rcloneroot
+    rclone --config ${CONFIG_PATH} \
+    --drive-chunk-size=32M move $_filename4 ${destpath} \
+    -v --stats=60s --transfers=16 --checkers=16
 fi
